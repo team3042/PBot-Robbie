3bfc2f87e3a00fff2c05725d911f68a1f392ce34
Robbie <Robbie@10.0.1.49>
2017-11-23 23:11:10 -0600

    Decided on comment format.

--- src/org/usfirst/frc/team3042/robot/FileIO.java
+++ src/org/usfirst/frc/team3042/robot/FileIO.java
@@ -4,19 +4,19 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
-/**--------------------------------------------------------------------------------
+/** FileIO ********************************************************************
  * Create and write to a text file.
- */
+ ******************************************************************************/
 public class FileIO {
-
+	
 	PrintStream writer;
 
-	/**---------------------------------------------------------------------------
+	/** openFile **************************************************************
 	 * Create a new file. If the file already exists it will get over-written.
 	 * 
-	 * @param path 		specifies the directory and should include the trailing "/"
-	 * @param filename 	specifies the name of the file to be created.
-	 */
+	 * String path 		the directory path; should include the trailing "/"
+	 * String filename 	the name of the file to be created
+	 **************************************************************************/
 	public void openFile(String path, String filename) {
 		File dir = new File(path);
 		String url = path + filename;
@@ -33,20 +33,19 @@ public class FileIO {
 		}
 	}
 
-	/**---------------------------------------------------------------------------
-	 * Write a line of text to the file.
-	 * Each call creates a new line.
+	/** writeToFile ***********************************************************
+	 * Write a line of text to the file. Each call creates a new line.
 	 * 
-	 * @param content contains the text to be appended to the file.
-	 */
+	 * String content 	Contains the text to be appended
+	 **************************************************************************/
 	public void writeToFile(String content) {
 		writer.println(content);
 		writer.flush();
 	}
 
-	/**---------------------------------------------------------------------------
+	/** closeFile *************************************************************
 	 * Close the text file.
-	 */
+	 **************************************************************************/
 	public void closeFile() {
 		writer.close();
 	}
--- src/org/usfirst/frc/team3042/robot/Logger.java
+++ src/org/usfirst/frc/team3042/robot/Logger.java
@@ -4,19 +4,29 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
+/** Logger ********************************************************************
+ * Creates specific formatting of output for the log file.
+ ******************************************************************************/
 public class Logger extends FileIO {
 	private static final String FILE_DATE_FORMAT = "yyyy-MM-dd-hhmmss";
 	private static final String LOG_TIME_FORMAT = "hh:mm:ss:SS";
-	
-	boolean useConsole;
-	boolean useFile;
-	int level;
-	
+
+	// Directory path where logs can be stored on the RoboRIO
 	private static final String path = "/home/lvusers/logs/";
 	
-	public Logger(boolean useConsole,boolean useFile,int level) {
+	boolean useConsole, useFile;
+	int level;
+		
+	/** Logger ****************************************************************
+	 * Create a new log file with name based on the current date and time.
+	 * 
+	 * boolean 	useConsole	true if output should be directed to the console
+	 * boolean 	useFile		true if output should be directed to the file
+	 * int 		level		the logging level; all statements less than or 
+	 * 						equal to this value will be printed
+	 **************************************************************************/
+	public Logger(boolean useConsole, boolean useFile, int level) {
 		if(useFile) {
-			// Naming file with timestamp
 			Date now = new Date();
 			SimpleDateFormat fileTimeStamp = new SimpleDateFormat(FILE_DATE_FORMAT);
 			fileTimeStamp.setTimeZone(TimeZone.getTimeZone("America/Chicago"));
@@ -30,30 +40,29 @@ public class Logger extends FileIO {
 		this.level = level;
 	}
 	
-	public void log(String message, int level) {
-			
+	/** log ***********************************************************************
+	 * Write an entry into the log
+	 * 
+	 * String 	message	the contents of the log entry
+	 * int 		level	the logging level of the entry
+	 ******************************************************************************/
+	public void log(String message, int level) {			
 		if(level <= this.level) {
-			// Getting and adding class name to log
+			// Add the name of the calling class to the message
 			String cls = Thread.currentThread().getStackTrace()[2].getClassName();
 			int i = cls.lastIndexOf(".") + 1;
 			cls = cls.substring(i);
-				
-			message = "[" + cls + "] " + message;
-				
-			// Adding timestamp to log
+								
+			//Add timestamp to the message
 			Date now = new Date();
 			SimpleDateFormat logTimeStamp = new SimpleDateFormat(LOG_TIME_FORMAT);
 			logTimeStamp.setTimeZone(TimeZone.getTimeZone("America/Chicago"));
 			String time = logTimeStamp.format(now);			
 							
-			message = time + "\t" + message;
+			message = time + "\t" + "[" + cls + "] " + message;
 				
-			if(useFile) {
-				this.writeToFile(message)     ;
-			}
-			if(useConsole) {
-				System.out.println(message);
-			}
+			if(useFile) {this.writeToFile(message);}
+			if(useConsole) {System.out.println(message);}
 		}
 	}
 }
